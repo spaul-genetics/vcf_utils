8cf9011774825ff0787b178f965a3f00fd55ec87
Subrata Paul <subrata.paul@ucdenver.edu>
2022-08-10 20:35:26 -0400

    Create script_render_html.r

---
+++ script_render_html.r
@@ -0,0 +1,13 @@
+#!/usr/bin/env Rscript
+args = commandArgs(trailingOnly=TRUE)
+# test if there is at least one argument: if not, return an error
+if (length(args)==0) {
+  stop("At least one argument must be supplied (input file).n", call.=FALSE)
+} else if (length(args)==1) {
+  # default output file
+  args[2] = "out.html"
+}
+
+vcf_stats = args[1]
+
+rmarkdown::render('test_script_html.R', output_file = args[2])
